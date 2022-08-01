c2795ccc13d924c26ab9d7efc9fcb67003cad7a6
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-07-31 23:52:37 -0300

    feat: Adiciona Tela de Inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {
+  StatusBar,
+  Container,
+  Header,
+  LogoContainer,
+  Logo,
+  AppName,
+  SubTitle,
+} from "../../global/styles/inicio";
+
+import Botao from "../../components/Botao";
+
+export default function Inicio() {
+  return (
+    <Container>
+      <StatusBar backgroundColor="#5636d3" />
+      <Header>
+        <LogoContainer>
+          <Logo source={require("../../../assets/images/Finance.png")} />
+          <AppName>gofinances</AppName>
+        </LogoContainer>
+        <SubTitle>Controle suas finanças de forma muito simples</SubTitle>
+      </Header>
+      <Botao
+        title="Clique aqui para entrar"
+        funcao={() => {
+          alert("Clicou");
+        }}
+      ></Botao>
+    </Container>
+  );
+}
