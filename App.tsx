dd6f0c89c4698a7724f5ed15fdd2b022d6ab364a
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-01 00:42:58 -0300

    feat: Adiciona navegacao

--- App.tsx
+++ App.tsx
@@ -1,8 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Inicio from "./src/pages/Inicio";
+import Home from "./src/pages/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Navegacao = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Inicio"
+          component={Inicio}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default function App() {
-  return <Inicio></Inicio>;
+  return <Navegacao />;
 }
