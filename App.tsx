19be30b46e8745498132ee87c8f796a1872d35c6
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2022-08-03 13:46:33 -0300

    fix: Altera App.tsx para renderizar as rotas

--- App.tsx
+++ App.tsx
@@ -1,32 +1,6 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Inicio from "./src/pages/Inicio";
-import Home from "./src/pages/Home";
-
-const Stack = createNativeStackNavigator();
-
-const Navegacao = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Inicio"
-          component={Inicio}
-          options={{ headerShown: false }}
-        />
-
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+import Routes from "./src/pages/Routes";
 
 export default function App() {
-  return <Navegacao />;
+  return <Routes />;
 }
